42c6ec16c9b21a28b3820738f212472ca9800369
hadiloomehdi <hadiloomehdi@gmail.com>
2019-04-11 18:26:07 +0430

    ALU now has case 000

--- ALU.v
+++ ALU.v
@@ -1,11 +1,12 @@
 
 `timescale 1ns/1ns
 module ALU(input [15:0] ri,rj,input [2:0] func,output [15:0] out,output zero);
-  assign out = (func == 3'b 000)? ri + rj:
-             (func == 3'b 001)? ri - rj:
-             (func == 3'b 010)? ri & rj:
-             (func == 3'b 011)? ri | rj:
-             (func == 3'b 100)? ~rj:
+  assign out = (func == 3'b 000) ? rj:
+             (func == 3'b 001)? ri + rj:
+             (func == 3'b 010)? ri - rj:
+             (func == 3'b 011)? ri & rj:
+             (func == 3'b 100)? ri | rj:
+             (func == 3'b 101)? ~rj:
               3'bz;
   assign zero = (ri - rj == 16'b0)? 0:1; 
    
