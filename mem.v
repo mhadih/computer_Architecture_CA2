d35441216b14ed264458245836b6bbb2db507dac
hadiloomehdi <hadiloomehdi@gmail.com>
2019-04-14 09:26:34 +0430

    merge

--- mem.v
+++ mem.v
@@ -1,6 +1,7 @@
 `timescale 1ns/1ns
 module memory(input [9:0] adr,input [15:0] data,input memwen,clk,output resMem);
   reg [15:0] m [0:1023];
+<<<<<<< Updated upstream
   
   initial begin
     
@@ -16,6 +17,20 @@ module memory(input [9:0] adr,input [15:0] data,input memwen,clk,output resMem);
     m[19] = 10;
   end
 
+=======
+  initial begin
+    m[100] = 1;
+    m[101] = 2;
+    m[102] = 3;
+    m[103] = 4;
+    m[104] = 5;
+    m[105] = 6;
+    m[106] = 7;
+    m[107] = 8;
+    m[108] = 9;
+    m[109] = 10;
+  end
+>>>>>>> Stashed changes
   always @(posedge clk) begin
     if(memwen) begin
       m[adr] <= data;
